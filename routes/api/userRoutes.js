a7d82d206832c8a0b8d1c1a10aef112f5bae9721
RLCastaldy <rlcastaldy@gmail.com>
2024-03-11 16:53:07 -0500

    all user routes working

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -43,5 +43,42 @@ router.post('/', async (req, res) => {
   }
 });
 
+// update user
+router.put('/:id', async (req, res) => {
+  try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({ message: 'Invalid Object ID!' });
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'No user found with this id!' });
+    }
+
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json({ message: 'Internal Server Error', error: err });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({ message: 'Invalid Object ID!' });
+    }
+
+    const user = await User.findOneAndDelete({ _id: req.params.id });
+
+    if (!user) {
+      return res.status(404).json({ message: 'No user found with this Object ID!' });
+    }
+
+    res.status(200).json({ message: 'User deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ message: 'Internal Server Error', error: err });
+  }
+});
+
 
 module.exports = router;
